4729900457b5f58a125b9519bc4be263c2fa86ba
Baturpcu <batusalkim11@gmail.com>
2022-11-24 02:43:12 +0300

    update
    update

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import TodoField from './components/TodoField';
-
+import { Todos   } from './components/Todos';
 function App() {
   
+  
+
   return(
     <div className='app'>
       <h1>ToDo List</h1>
       <TodoField/>
+      <Todos />
     </div>
   );
 
